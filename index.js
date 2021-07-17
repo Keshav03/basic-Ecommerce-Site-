c7a57728bd2b8b4658bae5a4aace530cb2c4298a
KeshavBokhoree <60392137+Keshav03@users.noreply.github.com>
2021-07-17 18:09:26 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+let products=[
+            {
+                productID=1,
+                productName = "Black Shirt",
+                productImage = "images/blackT-shirt.png",
+                productPrice= "$15",
+            },
+            {
+                productID=2,
+                productName = "Oxford Shirt",
+                productImage = "images/dressShirt.png",
+                productPrice= "$45",
+            },
+            {
+                productID=3,
+                productName = "Braithwait Watch",
+                productImage = "images/watch.png",
+                productPrice= "$199"
+            },    
+            {
+                productID=4,
+                productName = "Sunglasses",
+                productImage = "images/sunglasses.png",
+                productPrice= "$59",
+            },
+            {
+                productID=5,
+                productName = "White Shirt",
+                productImage = "images/whiteShirt.png",
+                productPrice= "$25",
+            },
+            {
+                productID=6,
+                productName = "Leather Watch",
+                productImage = "images/watch2.png",
+                productPrice= "$70",
+            },
+            {
+                productID=7,
+                productName = "Black Jean",
+                productImage = "images/blackjean.png",
+                productPrice= "$35",
+            }
+]
